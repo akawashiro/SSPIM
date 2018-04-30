84f3f9b699fb09db0fee4941c79d9e2231d2835f
akira <akirakawata1@gmail.com>
2018-05-01 00:02:03 +0900

    Made basic bit calculation.

--- main.rs
+++ main.rs
@@ -1,9 +1,12 @@
-type RegId = usize;
-type Word = i32;
+type RegId                = usize;
+type Word                 = i32;
+type Const                = i32;
+const N_REGISTER : usize  = 32;
+const MEMORY_SIZE : usize = (1<<16);
 
 struct MachineState{
-    register:[Word; 32 ],
-    memory:[Word; (1<<16) ]
+    register:[Word; N_REGISTER ],
+    memory:[Word; MEMORY_SIZE ]
 }
 
 enum Instruction{
@@ -17,14 +20,14 @@ enum Instruction{
     Nor(RegId,   RegId, RegId),
     Slt(RegId,   RegId, RegId),
     Sltu(RegId,  RegId, RegId),
-    Addi(RegId,  RegId, RegId),
-    Addiu(RegId, RegId, RegId),
-    Slti(RegId,  RegId, RegId),
-    Sltiu(RegId, RegId, RegId),
-    Andi(RegId,  RegId, RegId),
-    Ori(RegId,   RegId, RegId),
-    Xori(RegId,  RegId, RegId),
-    Lui(RegId,   RegId, RegId)
+    Addi(RegId,  RegId, Const),
+    Addiu(RegId, RegId, Const),
+    Slti(RegId,  RegId, Const),
+    Sltiu(RegId, RegId, Const),
+    Andi(RegId,  RegId, Const),
+    Ori(RegId,   RegId, Const),
+    Xori(RegId,  RegId, Const),
+    Lui(RegId,   Const)
 }
 
 fn execute(ms: &mut MachineState, ist: Instruction){
@@ -37,14 +40,33 @@ fn execute(ms: &mut MachineState, ist: Instruction){
         And(r1,r2,r3)  => ms.register[r1] = ms.register[r2] & ms.register[r3],
         Or(r1,r2,r3)   => ms.register[r1] = ms.register[r2] | ms.register[r3],
         Xor(r1,r2,r3)  => ms.register[r1] = ms.register[r2] ^ ms.register[r3],
-        _              => println!("Hello")
+        Nor(r1,r2,r3)  => ms.register[r1] = !(ms.register[r2] | ms.register[r3]),
+        Slt(r1,r2,r3)  => ms.register[r1] = (ms.register[r2] < ms.register[r3]) as Word,
+        Sltu(r1,r2,r3) => ms.register[r1] = (ms.register[r2] < ms.register[r3]) as Word,
+        Addi(r1,r2,c)  => ms.register[r1] = ms.register[r2] + c,
+        Addiu(r1,r2,c) => ms.register[r1] = ms.register[r2] + c,
+        Slti(r1,r2,c)  => ms.register[r1] = (ms.register[r2] < c) as Word,
+        Sltiu(r1,r2,c) => ms.register[r1] = (ms.register[r2] < c) as Word,
+        Andi(r1,r2,c)  => ms.register[r1] = ms.register[r2] & c,
+        Ori(r1,r2,c)   => ms.register[r1] = ms.register[r2] | c,
+        Xori(r1,r2,c)  => ms.register[r1] = ms.register[r2] ^ c,
+        Lui(r1,c)      => ms.register[r1] |= c << 16,
+    }
+}
+
+fn show(ms: &MachineState){
+    for i in 0..N_REGISTER {
+        print!("register[{}]={} ",i, ms.register[i]);
+        if i%4 == 3 {
+            print!("\n");
+        }
     }
 }
 
 fn main() {
+    use Instruction::*;
     let mut ms = MachineState { register : [0; 32], memory : [0; (1<<16)] };
-    ms.register[1] = 10;
-    let i = Instruction::Add(1,1,1);
-    println!("Hello")
+    execute(&mut ms, Addi(0,1,10));
+    show(&ms);
 }
 
